dbb0ef9a3512b1978ac28c727502d7d565da2a14
github username <github email address>
2020-06-26 17:09:44 -0400

    dropdown is WORKING and active storage imgs rendering

--- src/components/Mushroom.js
+++ src/components/Mushroom.js
@@ -3,11 +3,11 @@ import React from 'react'
 class Mushroom extends React.Component {
     render() {
 
-        const { id, name, scientific_name, location, flavor, image, sendToMushShowPage } = this.props
+        const { id, name, scientific_name, location, flavor, image_url, sendToMushShowPage } = this.props
 
         return(
             <div className='card' onClick={() => sendToMushShowPage(id)}>
-                <img src={image} alt={name} className='mush-img'/>
+                <img src={'http://localhost:3000' + image_url} alt={name} className='mush-img'/>
                 <div className='name-container'>
                     <h3>{name}</h3>
                     {/* <p><em>{scientific_name}</em></p>
