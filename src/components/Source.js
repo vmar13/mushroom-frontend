a30dc2ea8112a80b3027a4edad6fc71e232f8df2
vmar13 <60378440+vmar13@users.noreply.github.com>
2020-06-28 14:18:36 -0400

    sources rendering properly

---
+++ src/components/Source.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Source = (props) => {
+
+    const { citation } = props
+
+        return(
+            <div>
+                {citation}<br></br>   
+            </div>
+        )
+    }
+
+export default Source
---
+++ src/containers/SourcesContainer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Source from '../components/Source'
+
+const SourcesContainer = (props) => {
+
+    const { sources } = props
+console.log(props.sources)
+        return(
+            <div>
+                {sources.map(sourceArr => sourceArr.map(sourceObj => <Source key={sourceObj.id} {...sourceObj} />))}   
+            </div>
+        )
+    }
+
+export default SourcesContainer
