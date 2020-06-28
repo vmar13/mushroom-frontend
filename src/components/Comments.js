369febb872f810a320d5aa438fb512da54c2d08c
vmar13 <60378440+vmar13@users.noreply.github.com>
2020-06-27 23:27:32 -0400

    text to speech working

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,7 +1,20 @@
 import React from 'react'
 
 const Comments = () => {
-        return('Comment goes here')
+        return(
+            <div className="modal-guts">
+                    <form  >
+                        <div className='form-group'>
+                            <br></br>
+                            <br></br>
+                            <h3>Comments</h3>
+                            {/* <input name='name' placeholder="Dog Walker's Name"  /> */}
+                            <input type='text' name='comment'value='comment' placeholder="Share your opinion"  />
+                            <input type="submit" value="Submit" />
+                        </div>
+                    </form>
+            </div>
+        )
     }
 
 export default Comments
