89b146abbb4901d88d54734a951d98328c4473df
github username <60378440+vmar13@users.noreply.github.com>
2021-05-18 21:50:01 -0400

    grouped unit tests for mush info card into a describe func

--- src/components/MushShowPage.test.js
+++ src/components/MushShowPage.test.js
@@ -2,21 +2,29 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import MushShowPage from './MushShowPage';
 
-test('renders the label scientific name', () => {
-    //Arrange
-    render(<MushShowPage />);
+//Use describe function to group all unit tests belonging to 
+//one feature or component into a testing suite
+describe('renders mushroom info card', () => {
+    test('renders the scientific name label', () => {
+        //Arrange
+        render(<MushShowPage />);
+        //Act
+        //...nothing
+        //Assert
+        const sciName = screen.getByText(/Scientific Name/i);  //REGEX for case-insensitive
+        expect(sciName).toBeInTheDocument();
+    });
+
+    test('renders the location label', () => {
+        render(<MushShowPage />);
+        const location = screen.getByText(/Location/i);
+        expect(location).toBeInTheDocument();
+    })
     
-    //Act
-    //...nothing
+    test('renders the tea flavor label', () => {
+        render(<MushShowPage />);
+        const teaFlavor = screen.getByText(/Tea flavor/, { exact: true }); //Text must be exact match
+        expect(teaFlavor).toBeInTheDocument();
+    });
+})
 
-    //Assert
-    const sciName = screen.getByText(/Scientific Name/i);
-    expect(sciName).toBeInTheDocument();
-});
-
-test('renders the label tea flavor', () => {
-    render(<MushShowPage />);
-
-    const teaFlavor = screen.getByText(/Tea flavor/, { exact: true });
-    expect(teaFlavor).toBeInTheDocument();
-});
