86c34fb182f0d63fbd45c0b4109cb8ad427d434d
github username <60378440+vmar13@users.noreply.github.com>
2021-06-02 11:01:23 -0400

    refactored BYOT and searchBar comps for DRYcode

--- src/components/BYOT.js
+++ src/components/BYOT.js
@@ -1,15 +1,12 @@
-import React from 'react'
-import { Grid } from '@material-ui/core'
-import SearchBar from '../components/SearchBar'
-import VideoDetail from '../components/VideoDetail'
-import VideoList from '../components/VideoList'
+import React from 'react';
+import { Grid } from '@material-ui/core';
+import SearchBar from '../components/SearchBar';
+import VideoDetail from '../components/VideoDetail';
+import VideoList from '../components/VideoList';
 
-
-
-const REACT_APP_YOUTUBE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY
+const REACT_APP_YOUTUBE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 
 class BYOT extends React.Component {
-
 
     state = {
         videos: [],
@@ -17,25 +14,29 @@ class BYOT extends React.Component {
         searchTerm: ''
     }
 
-    handleSubmit = (searchTerm) => {
-        fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q=${searchTerm}&key=${REACT_APP_YOUTUBE_API_KEY}`)
+    handleChange = (event) => {
+        this.setState({ searchTerm: event.target.value });
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q=${this.state.searchTerm}&key=${REACT_APP_YOUTUBE_API_KEY}`)
             .then(res => res.json())
             .then(data => {
-                console.log(data)
-                // this.setState({ videos: data.items, selectedVideo: data.items[0], searchTerm: ''})
-                // console.log(data.items)
+                this.setState({ videos: data.items, selectedVideo: data.items[0], searchTerm: ''});
             })
         }
            
-    
         
     onVideoSelect = (video) => {
-        this.setState({ selectedVideo: video })
+        this.setState({ selectedVideo: video });
     }
     
     render() {
-        // console.log(this.props.videos)
-        // console.log(this.state.selectedVideo)
+        const { videos, selectedVideo } = this.state;
+        const { addFavVideo, favorited, toggleFavorited } = this.props;
+
+      
         return(
             <div className='byot-background'>
                 <Grid justify='center' container spacing={0}>
@@ -43,13 +44,13 @@ class BYOT extends React.Component {
                         <Grid container spacing={10}>
                             <h1 className='byot-title'>Brew Your Own Tea</h1>
                             <Grid item sm={12} style={{ padding: '40px' }}>
-                                <SearchBar  onSubmit={this.handleSubmit} />
+                                <SearchBar  onSubmit={this.handleSubmit} onChange={this.handleChange} />
                             </Grid>
                             <Grid item xs={8}>
-                                <VideoDetail video={this.state.selectedVideo} addFavVideo={this.props.addFavVideo} favorited={this.props.favorited} toggleFavorited={this.props.toggleFavorited}/>
+                                <VideoDetail video={selectedVideo} addFavVideo={addFavVideo} favorited={favorited} toggleFavorited={toggleFavorited}/>
                             </Grid>
                             <Grid item xs={4}>
-                                <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
+                                <VideoList videos={videos} onVideoSelect={this.onVideoSelect} />
                             </Grid>
                         </Grid>
                     </Grid>
@@ -59,4 +60,4 @@ class BYOT extends React.Component {
     }
 }
 
-export default BYOT
+export default BYOT;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,35 +1,18 @@
-import React from 'react'
+import React from 'react';
 
-class SearchBar extends React.Component {
+const SearchBar = (props) => {
+    const { onSubmit, onChange } = props; 
 
-    state = {
-        searchTerm: ''
-    }
+    return(
+        <>
+            <form onSubmit={onSubmit}  className='search'>
+                <input type='text' placeholder='Search...' onChange={onChange} />
+                <img src={require("../images/search.png")} alt="listen" className='search-magnifying-glass' />
+            </form>
+            <img src={require("../images/mush-excited.gif")} alt="listen" className='mush-excited' />
+        </>
+    )
 
-    handleChange = (event) => {
-        this.setState({ searchTerm: event.target.value })
-    }
-    
-    handleSubmit = (event) => {
-        const { searchTerm } = this.state 
-        const { onSubmit } = this.props  
-
-        onSubmit(searchTerm)
-
-        event.preventDefault()
-    }
-
-    render() {
-        return(
-            <>
-                    <form onSubmit={this.handleSubmit}  className='search'>
-                        <input type='text' placeholder='Search...' onChange={this.handleChange} />
-                        <img src={require("../images/search.png")} alt="listen" className='search-magnifying-glass' />
-                    </form>
-                    <img src={require("../images/mush-excited.gif")} alt="listen" className='mush-excited' />
-            </>
-        )
-    }
 }
 
-export default SearchBar
+export default SearchBar;
