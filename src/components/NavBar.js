83e43e8aa01ac3414073f387b752e4dbbdba775d
github username <60378440+vmar13@users.noreply.github.com>
2021-06-07 15:12:14 -0400

    added hamburger menu to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,9 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return(
       <div className='nav-container'>
+        <button id='hamburger'>
+          <i className='fas fa-bars'></i>
+        </button>
         <NavLink to='/mushrooms' className='nav-element'><strong>Mushroom Teas</strong></NavLink>
         <NavLink to='/byot' className='nav-element'><strong>BYOT</strong></NavLink> 
         <NavLink to='/favorites' className='nav-element'><strong>Favorites</strong></NavLink>
