05761b5eae88f90af29e620fbe517da5f1b0b832
github username <60378440+vmar13@users.noreply.github.com>
2021-06-29 13:03:33 -0400

    fixed byot search bar positioning and added instructions

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,6 +4,7 @@ const SearchBar = ({ onSubmit, onChange }) => {
     return(
         <>
             <form onSubmit={onSubmit}  className='search'>
+                <h4 className='search-instructions'>Find YouTube videos on how to brew your own mushroom tea. Ex: "How to make reishi tea"</h4>
                 <input type='text' placeholder='Search...' onChange={onChange} />
                 <img src={require("../images/search.png")} alt="listen" className='search-magnifying-glass' />
             </form>
